ee619567f680c439c59792dc28dc2fc8d0d721e3
Diogo Silva <dioowdioowtrd@gmail.com>
2025-12-26 13:50:32 -0300

    Atualização do service worker: cache preto e dourado

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,52 @@
-const cacheName = "reunioes-cache-v1";
+const cacheName = "celulas-atitude-cache-v1";
 const filesToCache = [
   "./",
   "./index.html",
   "./style.css",
-  "./manifest.json"
+  "./manifest.json",
+  "./icon.png",
+  "./logo.png"
 ];
 
+// Instalação e cache inicial
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(filesToCache);
+    })
   );
+  self.skipWaiting(); // ativa o SW imediatamente
 });
 
+// Ativação - remove caches antigos
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map((key) => {
+          if (key !== cacheName) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Intercepta requisições e responde com cache quando possível
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => {
+      return (
+        response ||
+        fetch(event.request).then((fetchResponse) => {
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        })
+      );
+    })
   );
 });
