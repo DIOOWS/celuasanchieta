6d339e28ace8b51ef7d43c0c5d05aa504ce6cfdf
Diogo Silva <dioowdioowtrd@gmail.com>
2025-12-26 11:56:45 -0300

    commitando 01

---
+++ service-worker.js
@@ -0,0 +1,19 @@
+const cacheName = "reunioes-cache-v1";
+const filesToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
